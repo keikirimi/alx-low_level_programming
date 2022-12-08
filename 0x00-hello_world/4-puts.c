1bfbd28ca76f30d91807c43cc070d902d844d4ae
keikirimi <keikirimi@gmail.com>
2022-12-08 10:44:21 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts(*\"Programming is like building a multilingual puzzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }

5f3826050cec691ebbff38347b7e320227ef1103
keikirimi <keikirimi@gmail.com>
2022-12-19 12:46:20 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I tear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}

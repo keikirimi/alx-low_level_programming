09ef4ec2a8ff1be9be9191dc79d496d30b341810
keikirimi <keikirimi@gmail.com>
2022-12-20 10:52:03 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string, starting with the \ first character, followed by a new line 
- *
+ * puts2 - prints every other character of a string, starting with the \ first character,
+ * followed by a new line
  * Return: nothing on success
  */
 
